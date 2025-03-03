319473933f6c7630e3a6909cd4a13a6f46236fd8
Dayvid0063 <orjidavid63@gmail.com>
2025-03-04 00:26:22 +0100

    style: created the app theme and colors

---
+++ MoveItApp/constants/theme.ts
@@ -0,0 +1,38 @@
+export const COLORS = {
+    primary: '#FF3B30',
+    secondary: '#FF6B66',
+    success: '#34C759',
+    danger: '#FF3B30',
+    warning: '#FF9500',
+    black: '#000000',
+    white: '#FFFFFF',
+    gray: {
+      100: '#F2F2F7',
+      200: '#E5E5EA',
+      300: '#D1D1D6',
+      400: '#C7C7CC',
+      500: '#AEAEB2',
+      600: '#8E8E93',
+      700: '#636366',
+      800: '#48484A',
+      900: '#3A3A3C'
+    }
+  };
+  
+  export const FONTS = {
+    regular: 'DMSans-Regular',
+    medium: 'DMSans-Medium',
+    bold: 'DMSans-Bold'
+  };
+  
+  export const SIZES = {
+    xs: 8,
+    sm: 12,
+    md: 16,
+    lg: 24,
+    xl: 32,
+    xxl: 40,
+    width: '100%',
+    height: '100%'
+  };
+  
