ec92c51f3b4602be2058dfa27ff21ee70abfc9e5
Dayvid0063 <orjidavid63@gmail.com>
2025-03-06 19:19:22 +0100

    updated server url for the mobile app

--- MoveItApp/api/client.ts
+++ MoveItApp/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://192.168.0.119:5000/api';
+const API_BASE_URL = 'https://moveit.chickenkiller.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
